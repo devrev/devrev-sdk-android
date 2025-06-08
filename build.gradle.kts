646875de2afdfb2ded52b79d774775d528722543
ArkadiGiniApps <arkadiy@gini-apps.com>
2025-06-08 10:28:39 +0300

    feature/ISS-156193-ingestion-redesign-kotlin-migration - preparatory work

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,11 @@ buildscript {
     dependencies {
         classpath(libs.kotlin.gradle.plugin)
         classpath(libs.android.gradlePlugin)
-        classpath("com.google.gms:google-services:4.3.8")
+        classpath("com.google.gms:google-services:4.4.2")
     }
 }
 
 plugins {
-    id("com.google.gms.google-services") version "4.3.8" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
